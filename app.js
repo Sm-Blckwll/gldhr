0b713cca8fe0ff6fd99a0ce88a396444ed432c89
Sm-Blckwll <61584982+Sm-Blckwll@users.noreply.github.com>
2025-01-28 16:47:31 +0000

    Add files via upload

--- app.js
+++ app.js
@@ -1,27 +1,44 @@
 $(document).ready(function () {
+    const date = new Date();
+
+    // Initialize map
+    const mapOptions = {
+        minZoom: 4,
+        maxZoom: 18,
+        center: [51.505, -0.09],
+        zoom: 5
+    };
+
+    const map = L.map('map', mapOptions);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
+        maxZoom: 18
+    }).addTo(map);
+
+    // Handle Start Button click
     $("#startButton").click(function () {
         $("#loading").delay(100).fadeOut("fast");
-    
-        //Google Tag script
+
         var script = document.createElement('script');
         script.src = 'https://www.googletagmanager.com/gtag/js?id=G-S5ZJW576QK';
         script.async = true;
         document.head.appendChild(script);
-    
-        //Google Tag
-        script.onload = function() {
+
+        script.onload = function () {
             window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
+            function gtag() { dataLayer.push(arguments); }
             gtag('js', new Date());
-    
             gtag('config', 'G-S5ZJW576QK');
         };
     });
 
+    // Handle Help Button click
     $("#help").click(function () {
         $("#theHelp").delay(100).fadeToggle("fast");
     });
 
+    // Handle Calendar Button click
     $("#cal").click(function () {
         $("<div id='calendar'></div>").dialog({
             modal: true,
@@ -45,121 +62,83 @@ $(document).ready(function () {
         });
     });
 
+    // Register Service Worker
     if ('serviceWorker' in navigator) {
         navigator.serviceWorker.register('/service-worker.js')
-          .then(function(registration) {
-            console.log('Service Worker registered with scope:', registration.scope);
-          })
-          .catch(function(error) {
-            console.log('Service Worker registration failed:', error);
-          });
+            .then(function (registration) {
+                console.log('Service Worker registered with scope:', registration.scope);
+            })
+            .catch(function (error) {
+                console.log('Service Worker registration failed:', error);
+            });
     }
 
-    document.getElementById('dateResult').innerHTML = `Date: ${date.toLocaleDateString('en-GB')}`;
-});
+    // Update date result
+    $('#dateResult').html(`Date: ${date.toLocaleDateString('en-GB')}`);
 
-$(document).ready(function () {
-    const date = new Date();
-
-    const mapOptions = {
-        minZoom: 4,
-        maxZoom: 18,
-        center: [51.505, -0.09],
-        zoom: 5
-    };
-
-    const pin = L.icon({
-        iconUrl: 'location-pin-solid.svg',
-        iconSize: [19, 47.5],
-        iconAnchor: [10, 37.5],
-        popupAnchor: [0, -34]
-    });
-
-    const map = L.map('map', mapOptions);
-
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
-        maxZoom: 18
-    }).addTo(map);
-
-    const goButton = document.getElementById("go");
-
-    goButton.addEventListener("click", async function () {
+    // Add Go button event listener
+    $("#go").click(function () {
         const center = map.getCenter();
-        await calculateGoldenHour(center);
+        calculateGoldenHour(center);
     });
 
-    async function loadTimeZoneData() {
-        const response = await fetch('timezones.json'); //fetching -.-
-        return await response.json();
+    // Load Timezone data using tz-lookup
+    async function loadTimeZoneData(lat, lng) {
+        const timeZone = tzlookup(lat, lng);
+        return timeZone || 'UTC'; // Fallback to 'UTC' if no time zone found
     }
 
-    async function findTimeZone(lat, lng, geoJsonData) {
-        const point = turf.point([lng, lat]);
-        for (const feature of geoJsonData.features) {
-            const polygon = feature.geometry;
-            if (turf.booleanPointInPolygon(point, polygon)) {
-                return feature.properties.tzid;
-            }
-        }
-        return 'Unknown TimeZone';
-    }
-
+    // Calculate Golden Hour
     async function calculateGoldenHour(latlng) {
-        document.getElementById('title').innerHTML = 'Calculating<span class="loading-text">.</span>';
-        document.getElementById('riseBar').style.background = "white";
-        document.getElementById('setBar').style.background = "white";
-        document.getElementById('goldenHours').style.color = "white";
+        
+        $('#riseBar').css("background", "white");
+        $('#setBar').css("background", "white");
+        $('#goldenHours').css("color", "white");
 
         let lat = latlng.lat;
         let lng = latlng.lng;
 
-        const geoJsonData = await loadTimeZoneData();
-        const timeZone = await findTimeZone(lat, lng, geoJsonData);
+        // Get Time Zone using tz-lookup
+        const timeZone = await loadTimeZoneData(lat, lng);
 
         const times = SunCalc.getTimes(date, lat, lng);
         const sunrise = new Date(times.sunrise);
         const sunset = new Date(times.sunset);
 
-        const goldenHourStartFrom = moment(sunrise).format('HH:mm');
-        const goldenHourStartTo = moment(sunrise).add(1, 'hour').format('HH:mm');
-        const goldenHourEndFrom = moment(sunset).subtract(1, 'hour').format('HH:mm');
-        const goldenHourEndTo = moment(sunset).format('HH:mm');
+        const goldenHourStartFrom = moment(sunrise).tz(timeZone).format('HH:mm');
+        const goldenHourStartTo = moment(sunrise).tz(timeZone).add(1, 'hour').format('HH:mm');
+        const goldenHourEndFrom = moment(sunset).tz(timeZone).subtract(1, 'hour').format('HH:mm');
+        const goldenHourEndTo = moment(sunset).tz(timeZone).format('HH:mm');
 
-        document.getElementById('goldenHours').style.color = "black";
+        $('#goldenHours').css("color", "black");
 
-        document.getElementById('riseFrom').innerHTML = `${goldenHourStartFrom}`;
-        document.getElementById('riseBar').style.background = "linear-gradient(90deg, rgba(255,146,146,1) 0%, rgba(255,223,140,1) 100%)";
-        document.getElementById('riseTo').innerHTML = `${goldenHourStartTo}`;
+        $('#riseFrom').html(goldenHourStartFrom);
+        $('#riseBar').css("background", "linear-gradient(90deg, rgba(255,146,146,1) 0%, rgba(255,223,140,1) 100%)");
+        $('#riseTo').html(goldenHourStartTo);
 
-        document.getElementById('setFrom').innerHTML = `${goldenHourEndFrom}`;
-        document.getElementById('setBar').style.background = "linear-gradient(270deg, rgba(255,146,146,1) 0%, rgba(255,223,140,1) 100%)";
-        document.getElementById('setTo').innerHTML = `${goldenHourEndTo}`;
+        $('#setFrom').html(goldenHourEndFrom);
+        $('#setBar').css("background", "linear-gradient(270deg, rgba(255,146,146,1) 0%, rgba(255,223,140,1) 100%)");
+        $('#setTo').html(goldenHourEndTo);
 
-        document.getElementById('dateResult').innerHTML = `Date: ${date.toLocaleDateString('en-GB')}`;
-        document.getElementById('zoneResult').innerHTML = `Time Zone: (${timeZone})`;
-        document.getElementById('title').innerHTML = 'Done!';
+        $('#dateResult').html(`Date: ${date.toLocaleDateString('en-GB')}`);
+        $('#zoneResult').html(`Time Zone: (${timeZone})`);
+        
     }
+
+    // Add Geocoder
+    var osmGeocoder = new L.Control.OSMGeocoder({
+        collapsed: true,
+        position: 'topleft',
+        text: '-',
+        placeholder: '',
+        bounds: null,
+        callback: function (results) {
+            var bbox = results[0].boundingbox,
+                first = new L.LatLng(bbox[0], bbox[2]),
+                second = new L.LatLng(bbox[1], bbox[3]),
+                bounds = new L.LatLngBounds([first, second]);
+            map.fitBounds(bounds);
+        }
+    });
+    map.addControl(osmGeocoder);
 });
-
-
-L.geolet({ position: 'bottomleft', className: 'locDot', popupContent: function (latlng) { return 'Your location: ' + latlng.lat + ', ' + latlng.lng; } }).addTo(map);
-
-var options = {
-    collapsed: true,
-    position: 'topleft',
-    text: '-',
-    placeholder: '',
-    bounds: null,
-    email: null,
-    callback: function (results) {
-        var bbox = results[0].boundingbox,
-            first = new L.LatLng(bbox[0], bbox[2]),
-            second = new L.LatLng(bbox[1], bbox[3]),
-            bounds = new L.LatLngBounds([first, second]);
-        this._map.fitBounds(bounds);
-    }
-};
-
-var osmGeocoder = new L.Control.OSMGeocoder(options);
-map.addControl(osmGeocoder);
